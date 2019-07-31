8b5031ea7dae7cc5516c63db793810d38c848e7f
Erby Fischer <erbycfischer@mac.com>
2019-07-31 10:12:09 -0500

    added user authentication with devise_token

--- loophole_api/app/controllers/application_controller.rb
+++ loophole_api/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+        include DeviseTokenAuth::Concerns::SetUserByToken
+        protect_from_forgery unless: -> { request.format.json? }
+        skip_before_action :verify_authenticity_token
 end
